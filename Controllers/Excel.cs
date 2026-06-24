93b1765bac30303c681fe778f280fcc449f82743
Nonso <onyeokorokennedy@gmail.com>
2026-06-24 12:55:59 +0100

    Add project files.

---
+++ Controllers/Excel.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+
+[ApiController]
+[Route("api/report")]
+public class UploadExcelController : ControllerBase
+{
+    [HttpPost("upload")]
+    public async Task<IActionResult> UploadExcel(IFormFile file)
+    {
+        if (file == null || file.Length == 0)
+            return BadRequest("No file uploaded");
+
+        var path = @"C:\Users\onyeo\Documents\Book1.xlsx";
+
+        using var stream = new FileStream(path, FileMode.Create);
+        await file.CopyToAsync(stream);
+
+        return Ok("Upload successful");
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,33 @@
+using ClosedXML.Excel;
+using Swashbuckle.AspNetCore;
+using Microsoft.AspNetCore.Mvc;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddControllers();
+
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerGen();
+
+var app = builder.Build();
+
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint(
+            "/swagger/v1/swagger.json",
+            "Excel API V1");
+    });
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
