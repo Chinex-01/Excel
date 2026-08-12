72adbb9316f55ab3a52b7243a5b1d45ef8709d2d
Nonso <onyeokorokennedy@gmail.com>
2026-08-12 11:04:44 +0100

    KL

---
+++ File manger/LoginRequest.cs
@@ -0,0 +1,11 @@
+using System.Text.Json.Serialization;
+
+namespace Excel.File_manger
+{
+    public class LoginRequest
+    {
+        public string Username { get; set; }
+        public string password { get; set; }
+
+    }
+}
